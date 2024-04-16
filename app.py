a0b6c2c589fbc608738a52eb152af94977ec64b2
CanadianGamer <39717944+EasyCanadianGamer@users.noreply.github.com>
2024-04-15 22:04:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 from IPython.display import display, Markdown
 from flask import Flask, request, render_template, redirect, url_for
 
-GOOGLE_API_KEY = "AIzaSyCshLoylaBl1E7CsF86vytLVpr4Nx2HxZw"
+GOOGLE_API_KEY = "API_KEY_HERE"
 genai.configure(api_key=GOOGLE_API_KEY)
 MODEL_NAME = "gemini-1.5-pro-latest"
 
